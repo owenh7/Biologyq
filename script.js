c93f400f544ad5acea6fc84270f1ec7d7f1c8f8d
BishopH7 <77747379+BishopH7@users.noreply.github.com>
2021-02-05 11:24:16 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
  $("#answer1").hide();
   $("#answer2").hide();
+   $("#answer3").hide();
+    $("#answer4").hide();
+     $("#answer5").hide();
+      $("#answer6").hide();
    $("#Hide_All").click(function(){
     $("#answer1").hide();
       $("#answer2").hide();
@@ -11,4 +15,15 @@ $(document).ready(function(){
   $("#Answer2").click(function(){
        $("#answer2").show();
 });
+  $("#Answer3").click(function(){
+       $("#answer4").show();
 });
+ $("#Answer4").click(function(){
+       $("#answer4").show();
+ });
+  $("#Answer5").click(function(){
+       $("#answer5").show();
+  });
+  $("#Answer6").click(function(){
+       $("#answer6").show();
+  });
